21ea19dc4e78b4dd37d0930e341740d0d739ff6d
Pedro <awe.ai.pedro@gmail.com>
2025-01-04 22:04:11 +0800

    [feature] reduce sqlalchemy logs

--- awe/db.py
+++ awe/db.py
@@ -2,7 +2,8 @@ from awe.settings import settings
 from sqlmodel import create_engine
 import logging
 
-logger = logging.getLogger("sqlalchemy.engine")
-logger.setLevel(settings.log_level)
+if settings.log_level == "DEBUG":
+    logger = logging.getLogger("sqlalchemy.engine")
+    logger.setLevel(settings.log_level)
 
 engine = create_engine(settings.db_connection_string)
